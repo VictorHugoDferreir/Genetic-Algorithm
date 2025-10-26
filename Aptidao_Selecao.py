e027601b7b1da8d233d4e1c69074a8bd453b1f24
victorhugo <victorhugodutraferreira@gmail.com>
2025-10-26 18:54:21 -0300

    Funções de Aptidão e Seleção atualizadas 2.0

--- Aptidao_Selecao.py
+++ Aptidao_Selecao.py
@@ -19,12 +19,16 @@ def avaliaPopulacao(populacao, BITS):
     for cromossoma in populacao:
         aptidao = calculaAptidao(cromossoma, BITS)
         aptidoes.append(aptidao)
+    return aptidoes
 
  
 def selecaoRoleta(aptidoes, populacao):
  
     soma_aptidoes = sum(aptidoes)
     selecao = random.uniform(0, soma_aptidoes)
+    somatorio = 0
+    
+#    print("Seleção por roleta - valor sorteado:", selecao) para ver o valor sorteado na roleta
     
     for aptidao in aptidoes:
         somatorio += aptidao
